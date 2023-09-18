00538e10a6d488d190a7771a73d123db64c8cda3
oftentaken <mario8000@gmail.com>
2023-09-18 23:02:43 +0200

    Settings changes

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-s=*x6$54(h22x)ng+opyjmv$3z_wzh9p_g%&6+ps-qlmldce9)'
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["217.182.76.101"]
+ALLOWED_HOSTS = ["217.182.76.101", "127.0.0.1"]
 
 
 # Application definition
