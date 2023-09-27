7d07739eb29cc0a37c766defe79664f265d4ccbb
oftentaken <mario8000@gmail.com>
2023-09-27 14:28:41 +0200

    Small changes

--- myblog/static/menu.js
+++ myblog/static/menu.js
@@ -7,18 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function adjustFooterPosition() {
-        const contentHeight = document.querySelector('.container').offsetHeight;
-        const viewportHeight = window.innerHeight;
-        const footer = document.querySelector('footer');
-
-        if (contentHeight < viewportHeight) {
-            footer.style.position = 'fixed';
-            footer.style.bottom = '0';
-        } else {
-            footer.style.position = 'static';
-        }
-    }
-
-    window.addEventListener('resize', adjustFooterPosition);
-    window.addEventListener('load', adjustFooterPosition);
+//function adjustFooterPosition() {
+//        const contentHeight = document.querySelector('.container').offsetHeight;
+//        const viewportHeight = window.innerHeight;
+//        const footer = document.querySelector('footer');
+//
+//        if (contentHeight < viewportHeight) {
+//            footer.style.position = 'fixed';
+//            footer.style.bottom = '0';
+//        } else {
+//            footer.style.position = 'static';
+//        }
+//    }
+//
+//    window.addEventListener('resize', adjustFooterPosition);
+//    window.addEventListener('load', adjustFooterPosition);
